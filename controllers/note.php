0a0400e2ed2309add1526fe73ff1472aa3b94a2a
robertsheikh <robertsheikh07@gmail.com>
2025-09-20 17:28:14 +0300

    9th

--- controllers/note.php
+++ controllers/note.php
@@ -4,8 +4,18 @@ $config =  require('config.php');
 $db = new Database($config['database']);
 
 $heading = "Note";
-$id = ($_GET['id']);
+$currentUserId = 1; 
+  
+$note = $db->query('select * from notes where id = :id',[  
+    'id'=>($_GET['id'])
+])->fetch();
 
-$note = $db->query('select * from notes where id= :id',['id'=>$id])->fetch();
+if(! $note){
+    abort();
+}
 
+if ($note [ 'user_id']!== $currentUserId){
+    abort(Response::FORBIDDEN);
+     
+}
 require "views/note.view.php";
---
+++ views/403.php
@@ -0,0 +1,13 @@
+<?php require "partials/head.php"; ?>
+ <?php require "partials/nav.php"; ?>
+
+  <main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+       <p class="text-2xl font-bold">you are not athorized to view thus page</p>
+    </div>
+    <p>
+        <a href="/" class='text-blue underline'>Go Back Home</a>
+    </p>
+  </main>
+
+  <?php require ("partials/footer.php"); ?>
